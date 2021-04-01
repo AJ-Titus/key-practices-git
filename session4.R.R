0f556b1c7b27573918dc4377ab44fefd61b57240
unknown <alex.titus@live.com>
2021-04-01 18:30:33 +0200

    Alex Titus R script

---
+++ session4.R.R
@@ -0,0 +1,11 @@
+x = log(22026.47)
+set.seed(x)
+sample(1:100, 10)
+y = 9
+#i = "ninth letter of the alphabet"
+help("read.table")
+#s = "first letter from see also function"
+citation()
+#r = "third word in citation section"
+
+#all words together equal ISR
